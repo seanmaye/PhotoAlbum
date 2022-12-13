4fa40b573cedecd6680373d687df69a6c7277825
smthe <sam710@scarletmail.rutgers.edu>
2022-12-13 16:38:26 -0500

    photos change

--- app/src/main/java/com/example/photos09/Photos.java
+++ app/src/main/java/com/example/photos09/Photos.java
@@ -24,7 +24,6 @@ private Button deleteButton;
 private ImageView imageView;
     ArrayList<String> listItems=new ArrayList<String>();
     ArrayAdapter<String> adapter;
-    int SELECT_PICTURE = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,50 +41,18 @@ private ImageView imageView;
                 listItems);
         listView.setAdapter(adapter);
 
-
-        // handle the Choose Image button to trigger
-        // the image chooser function
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                imageChooser();
+            public void onClick(View view) {
+                createAlbum();;
             }
         });
+
+    }
+    private void createAlbum() {
+        Intent switchActivityIntent = new Intent(this, CreateAlbum.class);
+        startActivity(switchActivityIntent);
     }
 
-    void imageChooser() {
-
-        // create an instance of the
-        // intent of the type image
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
-
-        // pass the constant to compare it
-        // with the returned requestCode
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
-    }
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-
-            // compare the resultCode with the
-            // SELECT_PICTURE constant
-            if (requestCode == SELECT_PICTURE) {
-                // Get the url of the image from data
-                Uri selectedImageUri = data.getData();
-                if (null != selectedImageUri) {
-                    // update the preview image in the layout
-                    listItems.add(selectedImageUri.toString());
-                    adapter=new ArrayAdapter<String>(this,
-                            android.R.layout.simple_list_item_1,
-                            listItems);
-                    listView.setAdapter(adapter);
-                    imageView.setImageURI(selectedImageUri);
-                }
-            }
-        }
-    }
 
 }
